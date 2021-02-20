6dda7c253bc45a604e4b62f74a5aa2b1b97c975f
Oriana <oriana.nohemi22@gmail.com>
2021-02-20 15:09:29 -0500

    pintar los gif que se reciben de la api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import getGifs from './services/getGifs';
 
 function App() {
+  const [ gifs, setGifs ] =  useState([])
+  // [] es un parametro para evitar que entre en un ciclo infinito
+  useEffect(() => {
+    getGifs()
+      .then(gifs => setGifs(gifs))
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {
+          gifs.map(singleGif => <img src={singleGif}/>)
+        }
+      </section>
     </div>
   );
 }
