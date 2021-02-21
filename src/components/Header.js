b1e8115d8474be4c30e8d303878763c5f5c57f78
Oriana <oriana.nohemi22@gmail.com>
2021-02-20 23:04:56 -0500

    mantener el header en ambas paginas

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import brand from '../assets/images/brand.png'
+import { Link } from "wouter";
+
 const Header = () => {
     return (
         <header className="header">
             <div className="header__brand-container">
                 <img className="responsive-image" src={brand}/>
             </div>
-            <h1>GiffyTown</h1>
+            <Link href="/">
+                <h1>GiffyTown</h1>
+            </Link>
         </header>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Header from '../components/Header'
 import Search from '../components/Search'
 import TrendingGifs from '../components/TrendingGifs'
 const Home = () => {
     return (
         <>
-            <Header/>
             <Search />
             <TrendingGifs/>
         </>
