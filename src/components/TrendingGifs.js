768dc1c374e19a9c9c56e6f6956d82970583fd3c
Oriana <oriana.nohemi22@gmail.com>
2021-02-21 23:20:42 -0500

    cambio de color del footer

--- src/components/TrendingGifs.js
+++ src/components/TrendingGifs.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getGifsTrending } from '../services/getGifs'
 import Gif from '../components/Gif'
-import Footer from './components/Footer'
+import Footer from '../components/Footer'
 
 const TrendingGifs = () => {
     const [ gifs, setGifs ] =  useState([])
